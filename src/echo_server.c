15e995407447bc3d93e5179a266acd2bbba169c8
GGGGGGGGGitHuhb <takanashilovesleep@gmail.com>
2026-02-15 12:36:58 +0800

    refactor: align fork echo server with UNP str_echo pattern

--- src/echo_server.c
+++ src/echo_server.c
@@ -1,8 +1,6 @@
 #include "unp.h"
 #include <errno.h>
 #include <signal.h>
-#include <stdio.h>
-#include <string.h>
 #include <strings.h>
 #include <sys/socket.h>
 #include <sys/wait.h>
@@ -16,33 +14,47 @@ static void sig_chld(int signo)
     }
 }
 
+static void str_echo(int sockfd)
+{
+    ssize_t n;
+    char recvline[MAXLINE];
+
+    while ((n = read(sockfd, recvline, MAXLINE)) > 0)
+    {
+        Writen(sockfd, recvline, (size_t) n);
+    }
+
+    if (n < 0)
+    {
+        err_sys("str_echo: read error");
+    }
+}
+
 int main(int argc, char **argv)
 {
-    int listenfd, connfd, n;
+    int listenfd, connfd;
     pid_t child_pid;
-    char buff[MAXLINE];
-    char recvline[MAXLINE + 1];
     struct sockaddr_in servaddr;
     struct sigaction sa;
 
     (void) argc;
     (void) argv;
 
-    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    listenfd = Socket(AF_INET, SOCK_STREAM, 0);
 
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(7);
 
-    bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
-    listen(listenfd, LISTENQ);
+    Bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
+    Listen(listenfd, LISTENQ);
 
     bzero(&sa, sizeof(sa));
     sa.sa_handler = sig_chld;
     sigemptyset(&sa.sa_mask);
     sa.sa_flags = SA_RESTART;
-    sigaction(SIGCHLD, &sa, NULL);
+    Sigaction(SIGCHLD, &sa, NULL);
 
     for (;;)
     {
@@ -56,25 +68,15 @@ int main(int argc, char **argv)
             err_sys("accept error");
         }
 
-        child_pid = fork();
-        if (child_pid < 0)
-            err_sys("fork error");
-
+        child_pid = Fork();
         if (child_pid == 0)
         {
-            close(listenfd);
-            while ((n = read(connfd, recvline, MAXLINE)) > 0)
-            {
-                recvline[n] = 0;
-                fputs(recvline, stdout);
-                strcpy(buff, recvline);
-                write(connfd, buff, strlen(buff));
-            }
-
-            close(connfd);
+            Close(listenfd);
+            str_echo(connfd);
+            Close(connfd);
             _exit(0);
         }
 
-        close(connfd);
+        Close(connfd);
     }
 }
